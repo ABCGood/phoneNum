c52deba9de872498c7668852345b7a974ae350b7
abcgoods <605797173@qq.com>
2018-06-20 10:05:33 +0800

    手机店员中心

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // autoplay: true,//可选选项，自动滑动
+    // slidesPerView: 'auto',
+    // loopedSlides: 2,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+})
